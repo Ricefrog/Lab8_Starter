4556b136b48dcf1b09eaaa571c047ff17d587833
Ricefrog <koacalloway@gmail.com>
2022-11-18 16:10:44 -0800

    Finished.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Lab 8 - Starter
 
+Koa Calloway
+
+## Check Your Understanding
+
 1) Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
 
 Answer: Within a Github action that runs whenever code is pushed.
@@ -9,3 +13,16 @@ Reasoning: This allows me to see the exact set of changes (conveniently grouped 
 2) Would you use an end to end test to check if a function is returning the correct output? (yes/no)
 
 Answer: No because this only tests the code and would be a unit test rather than an e2e test, which is concerned with UI.
+
+3) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+No because even if there are discrete inputs and outputs that could be tested in the code for this feature, it is intrinsically a UI feature. The things that should be tested are harder to put into a unit test; Is there lag when writing a message? Is the user able to use the feature effectively? Are there issues with the time it takes to send and receive messages? (The last one could hypothetically be measured with a unit test but there are many confounding factors such as the devices each user is using, their internet connection, location, etc). 
+
+4) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+This is appropriate for a unit test because it is discrete, quantifiable, and for the most part
+lives entirely in the code.
+
+## Screeenshot
+
+![tests](screenshot.png)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,83 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('310-555-7777 should be a phone number.', () => {
+    expect(functions.isPhoneNumber('310-555-7777')).toBe(true);
+});
+
+test('(310)555-7777 should be a phone number.', () => {
+    expect(functions.isPhoneNumber('(310)555-7777')).toBe(true);
+});
+
+test('310-555-777 should not be a phone number.', () => {
+    expect(functions.isPhoneNumber('310-555-777')).toBe(false);
+});
+
+test('7 should not be a phone number.', () => {
+    expect(functions.isPhoneNumber('7')).toBe(false);
+});
+
+test('greg@bean.com should be an email.', () => {
+    expect(functions.isEmail('greg@bean.com')).toBe(true);
+});
+
+test('g1337@g.mail should be an email.', () => {
+    expect(functions.isEmail('g1337@g.mal')).toBe(true);
+});
+
+test('@loooooooooong.b should not be an email.', () => {
+    expect(functions.isEmail('@loooooooooong.b')).toBe(false);
+});
+
+test('greg@google.123 should not be an email.', () => {
+    expect(functions.isEmail('greg@google.123')).toBe(false);
+});
+
+test('demonslayer32 should be a strong password.', () => {
+    expect(functions.isStrongPassword('demonslayer32')).toBe(true);
+});
+
+test('aaaa should be a strong password.', () => {
+    expect(functions.isStrongPassword('aaaa')).toBe(true);
+});
+
+test('32demonslayers should not be a strong password.', () => {
+    expect(functions.isStrongPassword('32demonslayers')).toBe(false);
+});
+
+
+test('13 should not be a strong password.', () => {
+    expect(functions.isStrongPassword('13')).toBe(false);
+});
+
+test('12/11/1999 should be a date.', () => {
+    expect(functions.isDate('12/11/1999')).toBe(true);
+});
+
+test('42/3/0000 should be a date.', () => {
+    expect(functions.isDate('42/3/0000')).toBe(true);
+});
+
+test('12-11-1999 should not be a date.', () => {
+    expect(functions.isDate('12-11-1999')).toBe(false);
+});
+
+test('12/11/99 should not be a date.', () => {
+    expect(functions.isDate('12/11/99')).toBe(false);
+});
+
+test('#ffffff should be a hex color.', () => {
+    expect(functions.isHexColor('#ffffff')).toBe(true);
+});
+
+test('fff should be a hex color.', () => {
+    expect(functions.isHexColor('fff')).toBe(true);
+});
+
+test('fffffh should not be a hex color.', () => {
+    expect(functions.isHexColor('fffffh')).toBe(false);
+});
+
+test('#9999z should not be a hex color.', () => {
+    expect(functions.isHexColor('#9999z')).toBe(false);
+});
